1765eba89657f1e37c3182bad4efe02665a3c5ee
Max Starr <max@maxstarr.xyz>
2025-02-01 21:26:48 -0600

    small layout fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -93,87 +93,86 @@
 </script>
 
 <!-- using tailwind -->
-<main
-	class="container mx-auto p-4"
-	style="display: grid; grid-template-columns: 1fr 1fr; gap: 1rem;"
->
+<main class="container mx-auto flex flex-col items-center justify-center">
 	<h1>Nerf Game</h1>
 
 	<aside class="card">
-		<h2>Testing Utilities</h2>
+		<h2 class="card-title">Testing Utilities</h2>
 
-		<div class="join join-horizontal">
-			<button class="btn btn-outline btn-primary join-item" on:click={() => testAudio('left')}
-				>Test Audio Left Channel</button
-			>
-
-			<button class="btn btn-outline btn-primary join-item" on:click={() => testAudio('right')}
-				>Test Audio Right Channel</button
-			>
-		</div>
-
-		<div class="join join-horizontal">
-			<select
-				class="join-item select select-bordered w-full max-w-xs"
-				bind:value={port}
-				on:change={(e) => setSerialPort()}
-			>
-				<option disabled selected>Select Port</option>
-				{#each serialPorts as port}
-					<option value={port}>{port.label}</option>
-				{/each}
-			</select>
-			<button class="btn btn-primary join-item" on:click={getSerialPorts}>
-				<!-- Refresh Icon -->
-
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="size-6"
-					class:loading-spin={loadingPorts}
+		<div class="card-body">
+			<div class="join join-horizontal">
+				<button class="btn btn-outline btn-primary join-item" on:click={() => testAudio('left')}
+					>Test Audio Left Channel</button
 				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-					/>
-				</svg>
-			</button>
-		</div>
 
-		<div class="join join-horizontal">
-			<select
-				class="join-item select select-bordered w-full max-w-xs"
-				bind:value={audioDevice}
-				on:change={(e) => setAudioDevice()}
-			>
-				<option disabled selected>Audio Device</option>
-				{#each audioDevices as device}
-					<option value={device}>{device.cardName} - {device.deviceName}</option>
-				{/each}
-			</select>
-			<button class="btn btn-primary join-item" on:click={getAudioDevices}>
-				<!-- Refresh Icon -->
-
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="size-6"
-					class:loading-spin={loadingAudio}
+				<button class="btn btn-outline btn-primary join-item" on:click={() => testAudio('right')}
+					>Test Audio Right Channel</button
 				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-					/>
-				</svg>
-			</button>
+			</div>
+
+			<div class="join join-horizontal">
+				<select
+					class="join-item select select-bordered w-full max-w-xs"
+					bind:value={port}
+					on:change={(e) => setSerialPort()}
+				>
+					<option disabled selected>Select Port</option>
+					{#each serialPorts as port}
+						<option value={port}>{port.label}</option>
+					{/each}
+				</select>
+				<button class="btn btn-primary join-item" on:click={getSerialPorts}>
+					<!-- Refresh Icon -->
+
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="size-6"
+						class:loading-spin={loadingPorts}
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+						/>
+					</svg>
+				</button>
+			</div>
+
+			<div class="join join-horizontal">
+				<select
+					class="join-item select select-bordered w-full max-w-xs"
+					bind:value={audioDevice}
+					on:change={(e) => setAudioDevice()}
+				>
+					<option disabled selected>Audio Device</option>
+					{#each audioDevices as device}
+						<option value={device}>{device.cardName} - {device.deviceName}</option>
+					{/each}
+				</select>
+				<button class="btn btn-primary join-item" on:click={getAudioDevices}>
+					<!-- Refresh Icon -->
+
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="size-6"
+						class:loading-spin={loadingAudio}
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+						/>
+					</svg>
+				</button>
+			</div>
 		</div>
 	</aside>
 </main>
