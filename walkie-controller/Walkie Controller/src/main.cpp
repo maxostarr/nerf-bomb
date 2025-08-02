a072b0f3969503a214eaea372cf228074b308baa
Max Starr <max@maxstarr.xyz>
2025-08-02 18:24:12 -0500

    update pins

--- walkie-controller/Walkie Controller/src/main.cpp
+++ walkie-controller/Walkie Controller/src/main.cpp
@@ -4,8 +4,8 @@
 // Left pin is D4
 // Right pin is D5
 
-const int LEFT_PIN = 23;
-const int RIGHT_PIN = 22;
+const int LEFT_PIN = 5;
+const int RIGHT_PIN = 3;
 const int BUFFER_SIZE = 64;
 
 char serialBuffer[BUFFER_SIZE];
@@ -21,42 +21,49 @@ enum Commands
 
 void processCommand(char command)
 {
-  Serial.write(command);
+  Serial.println(command);
 
   switch (command)
   {
   case ENABLE_LEFT:
     digitalWrite(LEFT_PIN, HIGH);
-    Serial.println("Left motor enabled");
+    Serial.println("Left walkie enabled");
     break;
   case ENABLE_RIGHT:
     digitalWrite(RIGHT_PIN, HIGH);
-    Serial.println("Right motor enabled");
+    Serial.println("Right walkie enabled");
     break;
   case DISABLE_LEFT:
     digitalWrite(LEFT_PIN, LOW);
-    Serial.println("Left motor disabled");
+    Serial.println("Left walkie disabled");
     break;
   case DISABLE_RIGHT:
     digitalWrite(RIGHT_PIN, LOW);
-    Serial.println("Right motor disabled");
+    Serial.println("Right walkie disabled");
     break;
   default:
+    Serial.println("Invalid command");
     break;
   }
+
+  Serial.print("Processed command: ");
+  Serial.println(command);
 }
 
 void setup()
 {
   pinMode(LEFT_PIN, OUTPUT);
   pinMode(RIGHT_PIN, OUTPUT);
-  Serial.begin(115200);
+  digitalWrite(LEFT_PIN, LOW);
+  digitalWrite(RIGHT_PIN, LOW);
+
+  Serial.begin(9600);
   while (!Serial)
     ;
 
   Serial.println("Hello world");
-  Serial.println("L - Enable left motor");
-  Serial.println("R - Enable right motor");
+  Serial.println("L - Enable left walkie");
+  Serial.println("R - Enable right walkie");
 }
 
 void loop()
@@ -68,6 +75,7 @@ void serialEvent()
 {
   while (Serial.available())
   {
+    Serial.println("Serial data available");
     char inChar = (char)Serial.read();
     if (inChar != '\n' && inChar != '\r')
     {
