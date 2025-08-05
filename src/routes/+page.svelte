799a1d1afa9110008efb05daa8f2548926572746
Max Starr <max@maxstarr.xyz>
2025-08-04 19:27:38 -0500

    refactor reload

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from 'svelte';
 	import { addToast } from '../lib/frontend/toastStore';
 	import * as io from '../lib/frontend/io.svelte';
+	import Reload from '../components/icons/reload.svelte';
 
 	let loadingPorts = false;
 	let loadingAudio = false;
@@ -108,23 +109,9 @@
 					{/each}
 				</select>
 				<button class="btn btn-primary join-item" on:click={getSerialPorts}>
-					<!-- Refresh Icon -->
-
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="size-6"
-						class:loading-spin={loadingPorts}
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-						/>
-					</svg>
+					<span class:loading-spin={loadingPorts}>
+						<Reload />
+					</span>
 				</button>
 			</div>
 
@@ -140,23 +127,9 @@
 					{/each}
 				</select>
 				<button class="btn btn-primary join-item" on:click={getAudioDevices}>
-					<!-- Refresh Icon -->
-
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="size-6"
-						class:loading-spin={loadingAudio}
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-						/>
-					</svg>
+					<span class:loading-spin={loadingAudio}>
+						<Reload />
+					</span>
 				</button>
 			</div>
 		</div>
