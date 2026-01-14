e8e126502c4609e97a81dc407a5b62cf0952a293
Max Starr <max@maxstarr.xyz>
2026-01-13 18:10:51 -0600

    rename component

--- src/lib/telemetry.ts
+++ src/lib/telemetry.ts
@@ -3,7 +3,7 @@ import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 
 export const NodeSdkLive = NodeSdk.layer(() => ({
-	resource: { serviceName: 'example' },
+	resource: { serviceName: 'effect' },
 
 	spanProcessor: new BatchSpanProcessor(new OTLPTraceExporter())
 }));
